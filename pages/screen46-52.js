45f8c531e9a5eef00f7d24a525295dadf12b56fd
VIKRAM THAKUR <rhydvik@gmail.com>
2018-05-05 14:24:51 +0530

    created new screens

---
+++ pages/screen46-52.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+export default class Nav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+                page 46-52
+            </div>
+        )
+    }
+}
