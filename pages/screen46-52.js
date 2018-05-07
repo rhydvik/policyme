654f8e517a001a47fc64bf7c1ca1635f9b0e1fbb
ankur010 <ankurjainms@gmail.com>
2018-05-07 10:51:05 +0530

    add button add in term length

--- pages/screen46-52.js
+++ pages/screen46-52.js
@@ -148,7 +148,12 @@ export default class Navy extends Component {
                                 <p className={styles.policyText}>Your term is the pre-agreed timeframe during which you will bw covered by the policy. For more insurance information on term insurance, click here. </p>
                                 <div className={styles.policyBox} >
                                     <p className={styles.quoteMessage}>Term Length</p>
-                                    <p className={styles.policyText}>30 year term</p>
+                                    <div  className={styles.termLengthBox} >
+                                        <Button buttonStyle={styles.termLengthInc} label="+"/>
+                                        <p className={styles.policyText}>30 year term</p>
+                                        <Button buttonStyle={styles.termLengthInc} label="-"/>
+                                    </div>
+                                    
                                     <p className={styles.policyText}>It look like your insurance needs are pretty steadyfor the next 30 years. We recommend buying a 30 year policy todayto protect your self. </p>
                                     <p className={styles.policyText}>Insurance is cancellable, so need to worry if you outgrow the protection.</p>
                                 </div>
